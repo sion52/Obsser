244f0060c7165d7d1be5049938d0b54e0a7bb67e
sion <kosion52@gmail.com>
2024-09-12 18:12:21 +0900

    keyword

--- lib/screens/hash_detail.dart
+++ lib/screens/hash_detail.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
 
-class HashDetail extends StatelessWidget {
+class HashDetail extends StatefulWidget {
+  @override
+  _HashDetailState createState() => _HashDetailState();
+}
+
+class _HashDetailState extends State<HashDetail> {
+  List<bool> isFavoriteList = [false, false, false, false]; // 각 카드의 즐겨찾기 상태를 저장하는 리스트
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFFFFFF),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(50, 50, 50, 16),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              buildKeywordHeader(), // 키워드 헤더 위젯
-              SizedBox(height: 8,),
-              buildKeywordChips(), // 키워드 칩 위젯
-            ],
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            buildKeywordHeader(),
+            SizedBox(height: 8,),
+            buildKeywordChips(),
+          ],
         ),
       ),
     );
   }
 
-  // 키워드별 여행지 헤더
   Widget buildKeywordHeader() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -44,25 +48,24 @@ class HashDetail extends StatelessWidget {
     );
   }
 
-  // 키워드 칩 리스트
   Widget buildKeywordChips() {
     List<String> keywords = ['#한적한', '#감성적인', '#휴양지', '#맛집', '#야경', '#여유로운', '#술', "#촬영지", "#드라이브", ];
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 0),
       child: Container(
-        height: 185, // 칩 리스트 높이
+        height: 700,
         width: double.infinity,
         decoration: BoxDecoration(
-          color: Color(0xFFFFFFFF), // 배경색
-          borderRadius: BorderRadius.circular(20), // 모서리 둥글게
+          color: Color(0xFFFFFFFF),
+          borderRadius: BorderRadius.circular(20),
           border: Border.all(
             width: 1,
-            color: Color(0xA6A4A4A4)
-          )
+            color: Color(0xA6A4A4A4),
+          ),
         ),
         child: Padding(
-          padding: EdgeInsets.fromLTRB(12, 14, 7, 0),
+          padding: EdgeInsets.fromLTRB(12, 14, 12, 14),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -75,12 +78,66 @@ class HashDetail extends StatelessWidget {
                 ),
                 alignment: Alignment.center,
                 child: Text(
-                  'text',
+                  keywords[0],
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400, color: Color(0xFF000000)),
                 ),
-              )
+              ),
+              SizedBox(height: 12,),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      _buildCard(0, '카멜리아 힐', 'assets/pictures/camellia.png'),
+                      SizedBox(height: 5,),
+                      _buildCard(1, '생각하는 정원', 'assets/pictures/garden.png'),
+                      SizedBox(height: 5,),
+                      _buildCard(2, '베케', 'assets/pictures/veke.png'),
+                      SizedBox(height: 5,),
+                      _buildCard(3, '삼성혈', 'assets/pictures/samsung.png'),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(int index, String title, String imageUrl) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 0,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Stack(
+          children: [
+            Image.asset(imageUrl, fit: BoxFit.cover, width: double.infinity, height: 220),
+            Positioned(
+              bottom: 10,
+              left: 15,
+              child: Text(title, style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.white)),
+            ),
+            Positioned(
+              bottom: 5,
+              right: 2,
+              child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    isFavoriteList[index] = !isFavoriteList[index]; // 해당 카드의 즐겨찾기 상태만 변경
+                  });
+                },
+                icon: Icon(
+                  isFavoriteList[index] ? Icons.favorite : Icons.favorite_border,
+                  size: 30,
+                  color: Color(0xFFFFFFFF),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
